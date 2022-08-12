3d710f69fef8fafeea4ed3ae99b9524583b37542
codevuive85 <111039361+codevuive85@users.noreply.github.com>
2022-08-12 14:29:10 +0700

    sua index.php
    ok lar !!!

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<center><b>continuous intergration for PHP projects using jenkins github</b></center>
 <h1> welcome jenkins CICD - github - jenkins</h1>
 <h2> CICD jenkins so easy </h2>
 <h3> Trien khai CICD cho EC2 apache </h3>
