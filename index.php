f8e31e0e06959f28bc58ffe83622c22f12bbbf0e
codevuive85 <111039361+codevuive85@users.noreply.github.com>
2022-08-12 14:04:15 +0700

    update. index.php
    cap nhat index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <h1> welcome jenkins CICD - github - jenkins</h1>
 <h2> CICD jenkins so easy </h2>
 <?php
-	echo "auto deploy code thanh cong roi";
+	echo "auto deploy code to EC2-web thanh cong roi";
 ?>
