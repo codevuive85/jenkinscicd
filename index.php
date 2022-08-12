9b4f4f6323a287f09e3a10120d2c515073738c40
codevuive85 <111039361+codevuive85@users.noreply.github.com>
2022-08-12 14:16:09 +0700

    cap nhat index.php
    sua loi

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <h1> welcome jenkins CICD - github - jenkins</h1>
 <h2> CICD jenkins so easy </h2>
+<h3> Trien khai CICD cho EC2 apache </h3>
 <?php
 	echo "auto deploy code to EC2-web-apache thanh cong roi";
 ?>
